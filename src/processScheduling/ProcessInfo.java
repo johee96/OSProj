d1c0edfb16d5eed74c1d8d9a8314bd0def044675
johee96 <whgml1@gmail.com>
2020-03-11 14:16:49 +0900

    code refactoring and add comment

--- src/processScheduling/ProcessInfo.java
+++ src/processScheduling/ProcessInfo.java
@@ -2,75 +2,193 @@ package processScheduling;
 
 import java.util.Comparator;
 
+/**
+ * Represents process information.
+ *
+ * @author cho hee
+ * @since 2020-03-11
+ */
 public class ProcessInfo implements Comparable<ProcessInfo> {
-    private int pId, arrival_time, burst_time, waiting_time, turnaround_time;
+    /**
+     * Represents process id.
+     */
+    private int pId;
+    /**
+     * Represents process arrival time.
+     */
+    private int arrivalTime;
+    /**
+     * Represents process burst time entered.
+     */
+    private int burstTime;
+    /**
+     * Represents process waiting time.
+     */
+    private int waitingTime;
+    /**
+     * The time it takes to start and complete the process.
+     */
+    private int turnaroundTime;
+    /**
+     * Represents normalized turnaround time.
+     */
     private double normalizedTT;
-    private int remainingBurst_time;
+    /**
+     * Represents the remaining process burst time.
+     */
+    private int remainingBurstTime;
 
-    public ProcessInfo(int pId, int arrival_time, int burst_time) {
-        this.arrival_time = arrival_time;
+    /**
+     * Create an process information.
+     *
+     * @param pId         the process id
+     * @param arrivalTime the process arrival time
+     * @param burstTime   the process burst time
+     */
+    public ProcessInfo(int pId, int arrivalTime, int burstTime) {
+        this.arrivalTime = arrivalTime;
         this.pId = pId;
-        this.burst_time = burst_time;
-        this.remainingBurst_time = burst_time;
+        this.burstTime = burstTime;
+        this.remainingBurstTime = burstTime;
     }
 
+    /**
+     * Gets the process id.
+     *
+     * @return A int representing the process id.
+     */
     public int getpId() {
-        return pId;
+        return this.pId;
     }
 
-    public int getRemainingBurst_time() {
-        return remainingBurst_time;
+    /**
+     * Gets the remaining process burst time.
+     *
+     * @return A int representing the remaining process burst time.
+     */
+    public int getRemainingBurstTime() {
+        return this.remainingBurstTime;
     }
 
-    public int getArrival_time() {
-        return arrival_time;
+    /**
+     * Gets the process arrival time.
+     *
+     * @return int representing the process arrival time.
+     */
+    public int getArrivalTime() {
+        return this.arrivalTime;
     }
 
-    public int getBurst_time() {
-        return burst_time;
+    /**
+     * Gets the process burst time.
+     *
+     * @return int representing the process burst time.
+     */
+    public int getBurstTime() {
+        return this.burstTime;
     }
 
-    public void setRemainingBurst_time(int tmpBurst_time) {
-        this.remainingBurst_time = tmpBurst_time;
+    /**
+     * Gets the process waiting time.
+     *
+     * @return A int representing the process waiting time.
+     */
+    public int getWaitingTime() {
+        return this.waitingTime;
     }
 
-    public int getWaiting_time() {
-        return waiting_time;
+    /**
+     * Gets the process turnaround time.
+     *
+     * @return A int representing the process turnaround time.
+     */
+    public int getTurnaroundTime() {
+        return this.turnaroundTime;
     }
 
-    public int getTurnaround_time() {
-        return turnaround_time;
+    /**
+     * Gets the process normalized turnaround time.
+     *
+     * @return A double representing the process normalized turnaround time.
+     */
+    public double getNormalizedTT() {
+        return this.normalizedTT;
     }
 
-    public void setWaiting_time(int waiting_time) {
-        this.waiting_time = waiting_time;
+    /**
+     * Sets the process burst time.
+     *
+     * @param burstTime A int containing the process burst time.
+     */
+    public void setRemainingBurstTime(int burstTime) {
+        this.remainingBurstTime = burstTime;
     }
 
-    public void setTurnaround_time(int turnaround_time) {
-        this.turnaround_time = turnaround_time;
+    /**
+     * Sets the process waiting time.
+     *
+     * @param waitingTime A int containing the process waiting time.
+     */
+    public void setWaitingTime(int waitingTime) {
+        this.waitingTime = waitingTime;
     }
 
+    /**
+     * Sets the process turnaround time.
+     *
+     * @param turnaroundTime A int containing the process turnaround time.
+     */
+    public void setTurnaroundTime(int turnaroundTime) {
+        this.turnaroundTime = turnaroundTime;
+    }
+
+    /**
+     * Sets the process normalized turnaround time.
+     *
+     * @param normalizedTT A double containing the process turnaround time.
+     */
     public void setNormalizedTT(double normalizedTT) {
         this.normalizedTT = normalizedTT;
     }
 
-    public double getNormalizedTT() {
-        return normalizedTT;
-    }
-
+    /**
+     * Sort by burst time for SPN Scheduling
+     *
+     * @param processInfo A ProcessInfo containing the Process information.
+     * @return A int containing comparative result
+     */
     @Override
-    public int compareTo(ProcessInfo o) {
+    public int compareTo(ProcessInfo processInfo) {
         //SPN_Scheduling를 위해 burst time 으로 정렬
-        if (this.burst_time < o.getBurst_time()) return -1;
-        else if (this.burst_time > o.getBurst_time()) return 1;
+        if (this.burstTime < processInfo.getBurstTime()) return -1;
+        else if (this.burstTime > processInfo.getBurstTime()) return 1;
         return 0;
     }
 
+    /**
+     * Sort by process id
+     */
     static Comparator<ProcessInfo> idComparator = (a, b) -> a.getpId() - b.getpId();
-
-    static Comparator<ProcessInfo> HRRNComparator = (a, b) -> {
-        int priorityA = (a.getWaiting_time() + a.getBurst_time()) / a.getBurst_time();
-        int priorityB = (b.getWaiting_time() + b.getBurst_time()) / b.getBurst_time();
-        return priorityA - priorityB;
+    /**
+     * Sort by response ratio in HRRN algorithm.
+     */
+    static Comparator<ProcessInfo> HRRNComparator = (o1, o2) -> {
+        int responseRatio1 = (o1.getWaitingTime() + o1.getRemainingBurstTime()) / o1.getRemainingBurstTime();
+        int responseRatio2 = (o2.getWaitingTime() + o2.getRemainingBurstTime()) / o2.getRemainingBurstTime();
+        return responseRatio2 - responseRatio1;
     };
+    /**
+     * Sort by remaining process burst time in SRTN algorithm.
+     */
+    static Comparator<ProcessInfo> SRTNComparator = (o1, o2) -> o1.getRemainingBurstTime() - o2.getRemainingBurstTime();
+    /**
+     * Calculate normalized turnaround time.
+     *
+     * @param processInfo A processInfo containing the process information.
+     * @return A double containing calculated result
+     */
+    static double calNTT(ProcessInfo processInfo) {
+        double nTT = (double) processInfo.getTurnaroundTime() / processInfo.getBurstTime();
+        return Math.round(nTT * 10) / 10.0;
+    }
 }
--- src/processScheduling/TestMain.java
+++ src/processScheduling/TestMain.java
@@ -2,23 +2,40 @@ package processScheduling;
 
 import java.util.ArrayList;
 
+/**
+ * Test process scheduling algorithms.
+ *
+ * @author cho hee
+ * @since 2020-03-11
+ */
 public class TestMain {
     public static void main(String[] args) {
         Scheduling scheduling = new Scheduling();
+
         ArrayList<ProcessInfo> FCFSSchedulingResult = scheduling.FCFS_Scheduling(getDummyProcess());
         printProcessInfo(FCFSSchedulingResult);
+
         ArrayList<ProcessInfo> RRSchedulingResult = scheduling.RR_Scheduling(getDummyProcess(), 4);
         printProcessInfo(RRSchedulingResult);
+
         ArrayList<ProcessInfo> SPNSchedulingResult = scheduling.SPN_Scheduling(getDummyProcess());
         printProcessInfo(SPNSchedulingResult);
+
         ArrayList<ProcessInfo> SRTNSchedulingResult = scheduling.SRTN_Scheduling(getDummyProcess());
         printProcessInfo(SRTNSchedulingResult);
+
         ArrayList<ProcessInfo> HRRNSchedulingResult = scheduling.HRRN_Scheduling(getDummyProcess());
         printProcessInfo(HRRNSchedulingResult);
     }
 
+    /**
+     * Create dummy datas (Process info).
+     * dummy datas are always sorted by arrival time.
+     * @return Created dummy datas
+     */
     private static ArrayList<ProcessInfo> getDummyProcess() {
         ArrayList<ProcessInfo> dummyProcessInfo = new ArrayList<>();
+
         dummyProcessInfo.add(new ProcessInfo(1, 0, 3));
         dummyProcessInfo.add(new ProcessInfo(2, 1, 7));
         dummyProcessInfo.add(new ProcessInfo(3, 3, 2));
@@ -27,15 +44,21 @@ public class TestMain {
         return dummyProcessInfo;
     }
 
+    /**
+     * Print process information and average response time.
+     * process information : pid(process id), arrival time, burst time, waiting time, turnaround time, normalizedTT
+     *
+     * @param processInfo to print
+     */
     private static void printProcessInfo(ArrayList<ProcessInfo> processInfo) {
         System.out.println("---------------------------------");
 
         double TTSum = 0;
         for (ProcessInfo tmp : processInfo) {
-            System.out.println("Pid: " + tmp.getpId() + " Arrival Time: " + tmp.getArrival_time()
-                    + " BurstTime: " + tmp.getBurst_time() + " WaitingTime: " + tmp.getWaiting_time() + " TurnaroundTime: " + tmp.getTurnaround_time()
+            System.out.println("Pid: " + tmp.getpId() + " Arrival Time: " + tmp.getArrivalTime()
+                    + " BurstTime: " + tmp.getBurstTime() + " WaitingTime: " + tmp.getWaitingTime() + " TurnaroundTime: " + tmp.getTurnaroundTime()
                     + " NormalizedTT: " + tmp.getNormalizedTT());
-            TTSum += tmp.getTurnaround_time();
+            TTSum += tmp.getTurnaroundTime();
         }
         System.out.println("Average response time : " + TTSum / processInfo.size());
 
